efb7253310492f1e9dec6e26c2312ab114f04356
rafabg7 <rafa_bg@outlook.com>
2019-04-03 15:58:25 -0600

    main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,12 @@
-jQuery(document).on('submit','#formlg',function(event){
+
+jQuery(document).on('submit','#formLg',function(event){
     event.preventDefault();
-
-    jQuery.ajay({
-        url:'login.php',
-        type: 'POST',
-        dataType: 'json',
-        data: $(this).serialize(),
-        beforeSend: function(){
-
+    jQuery.ajax({
+        url:'main_app/login.php',
+        type:'POST',
+        dataType:'json',
+        data:$(this).serialize(),
+        beforeSend:function(){
+          $('.botonlg').val('Validando....');
         }
-    })
-    .done(function(respuesta){
-        console.log("respuesta");
-    })
-    .fail(function(resp){
-        console.log("resp.responseText");
-    })
-    .always(function(){
-        console.log("complete");
-    });
-});
+      })
