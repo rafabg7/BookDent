a2594c5d7c792d6763af6cb7ec3512934b0abb64
rafabg7 <rafa_bg@outlook.com>
2019-04-03 14:55:47 -0600

    usurios

--- base/login.sql
+++ base/login.sql
@@ -1,4 +1,6 @@
 INSERT INTO `usuarios` (`id`, `nombres`, `usuario`, `password`, `tipo`) VALUES
 	(1, 'Angel Alberto', 'Angel', '123', 'Admin'),
     (2, 'Michelle Estefania\r\n', 'Michelle', '1234', 'Usuario'),
-    	(3, 'Michael Varela', 'Michael',
+    	(3, 'Michael Varela', 'Michael', '12344', 'Usuario'),
+        	(4, 'Jose Cardenas', 'Jose', '12345', 'Usuario'),
+            	(5, 'Jorge Remache', 'Jorge', '12345', 'Usuario');
