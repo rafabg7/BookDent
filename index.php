a65005d3e1ed9a54f77bbbbb35a400df461336d2
rafabg7 <rafa_bg@outlook.com>
2019-04-03 15:00:58 -0600

    Creacion de nuevo index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BookDent</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<div class="error">
+      <span>Datos de ingreso no válidos, inténtelo de nuevo  por favor</span>
+    </div>
+    
+</body>
+</html>
