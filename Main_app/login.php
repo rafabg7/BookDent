34fcb239332610a7410172e101197ff896cc2786
rafabg7 <rafa_bg@outlook.com>
2019-04-03 16:57:52 -0600

    Logueo con seguridad de administrador y usuarios

--- Main_app/login.php
+++ Main_app/login.php
@@ -1,28 +1,17 @@
 <?php
-
+require('conexion.php');
+sleep(2);
 $usu=$_POST['usuariolg'];
 $pass=$_POST['passlg'];
 $usuarios=$mysqli->query("Select nombres,tipo
                         From usuarios Where usuario='".$usu."'
                       AND password='".$pass."'");
-                      if ($usuarios->num_rows >0):
-                        $datos= $usuarios->fetch_assoc();
-                        echo json_encode(array('error'=>false,'tipo'=>$datos['tipo']));
-                    else:
-                        echo json_encode(array('error'=>true));
-                    endif;
-
-
-                    if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) $$ strolower($_SERVER['HTTP_X_REQUESTED_WTH']) == 'xmlhttprequest'){
-
-	require('conexion.php');
-    sleep(2);
-
-}
-
+if ($usuarios->num_rows==1):
+  $datos= $usuarios->fetch_assoc();
+    echo json_encode(array('error'=>false,'tipo'=>$datos['tipo']));
+else:
+    echo json_encode(array('error'=>true));
+endif;
 $mysqli->close();
+ ?>
 
-
-
-
-?>
--- js/main.js
+++ js/main.js
@@ -10,3 +10,27 @@ jQuery(document).on('submit','#formLg',function(event){
           $('.botonlg').val('Validando....');
         }
       })
+      .done(function(respuesta){
+        console.log(respuesta);
+        if (!respuesta.error) {
+            if (respuesta.tipo=='Admin') {
+                location='main_app/Admin/admin.php';
+              }else if (respuesta.tipo=='Usuario') {
+                location='main_app/Usuario/usuario.php';
+              }
+            }else {
+                $('.error').slideDown('slow');
+                setTimeout(function(){
+                $('.error').slideUp('slow');
+              },3000);
+              $('.botonlg').val('Iniciar Secion');
+              }
+            })
+            .fail(function(resp){
+              console.log(resp.responseText);
+            })
+            .always(function(){
+              console.log("complete");
+            });
+        });
+  
