895282240334273623894c8015857d09097bc898
rafabg7 <rafa_bg@outlook.com>
2019-04-08 00:12:29 -0600

    Registro

--- index.php
+++ index.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>BookDent</title>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <title>Login con PHP + MySQL + JQuery</title>
     <link rel="stylesheet" href="css/main.css">
-</head>
-<body>
-<div class="error">
+  </head>
+  <body>
+    <div class="error">
       <span>Datos de ingreso no válidos, inténtelo de nuevo  por favor</span>
     </div>
-    <input type="text" name="usuariolg" pattern="[A-Za-z0-9_]{1,20}" required  placeholder="Usuario" required>
-    <input type="text" name="passlg" pattern="[A-Za-z0-9_]{1,20} required placeholder="Contraseña" required>
+    <div class="main">
+     <form action="" id="formLg">
+        <input type="text" name="usuariolg"  placeholder="Usuario" required>
+        <input type="password" name="passlg"  placeholder="Contraseña" required>
         <input type="submit" class="botonlg"  value="Iniciar Sesion" >
      </form>
     </div>
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/main.js"></script>
-    
-</body>
+  </body>
 </html>
